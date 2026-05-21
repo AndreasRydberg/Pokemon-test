f346637edc66679e0a4ddf7767cf752241708377
Andreas Rydberg <andreas.rydberg.80@gmail.com>
2026-05-21 19:46:10 +0200

    Clean up code in Server class.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Application } from "express";
-// import cors, { CorsOptions } from "cors";
 
 export default class Server {
   constructor(app: Application) {
@@ -7,11 +6,6 @@ export default class Server {
   }
 
   private config(app: Application): void {
-    // const corsOptions: CorsOptions = {
-    //   origin: "http://localhost:8081"
-    // };
-
-    // app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   }
