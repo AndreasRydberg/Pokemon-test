e34447d186e61254857faed34a8403d74edc7910
Andreas Rydberg <andreas.rydberg.80@gmail.com>
2026-05-21 17:08:37 +0200

    Add comments about design decisions.

--- src/routes/pokemon.routes.ts
+++ src/routes/pokemon.routes.ts
@@ -12,6 +12,9 @@ class PokemonRoutes {
 
     intializeRoutes() {
         this.router.get("/index", list);
+
+        // Using GET for battle for simplicity, but in a real application this should
+        // probably be a POST with a request body containing the team data.
         this.router.get("/battle", battle);
     }
 }
